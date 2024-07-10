a3c3022915794665c910f7a4c69f77c2f9509a89
Simran <simran.bamania@mediamonks.com>
2024-07-10 16:24:30 +0530

    Updates

---
+++ precache-manifest.3dc6b777ecb977006d74fbb76a920644.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a832affb7795d327af29c11cc1370f6",
+    "url": "/SimranBamania/Cloths_Detection.git/index.html"
+  },
+  {
+    "revision": "c4729e10e74a077723f0",
+    "url": "/SimranBamania/Cloths_Detection.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "814535a2477bd5442f7c",
+    "url": "/SimranBamania/Cloths_Detection.git/static/js/2.cc85592c.chunk.js"
+  },
+  {
+    "revision": "bc620bd9cc8893328a5f453e25589d03",
+    "url": "/SimranBamania/Cloths_Detection.git/static/js/2.cc85592c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4729e10e74a077723f0",
+    "url": "/SimranBamania/Cloths_Detection.git/static/js/main.771c617b.chunk.js"
+  },
+  {
+    "revision": "25e5c8742c7e56fab341",
+    "url": "/SimranBamania/Cloths_Detection.git/static/js/runtime-main.659fa1ac.js"
+  }
+]);
